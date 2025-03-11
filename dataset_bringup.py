829fb764e8f3ec7ddc5bef9149e8762598726e15
PyoSH <jeongmok99@koreatech.ac.kr>
2025-03-11 21:08:04 +0900

    4개의 클래스 앞선 업데이트

--- dataset_bringup.py
+++ dataset_bringup.py
@@ -28,7 +28,7 @@ len_frame_time = cfg.HYPERPARAMS.LEN_FRAME * 0.001 # 100 ms = 0.1 s
 len_frame_sample = int(len_frame_time * sample_rate) # sample num = 2205, 100ms frame = 4410 samples.
 
 # dataPath = os.path.join('data', 'inspection' ,'unknown') # or 'train' !!!!!!
-dataPath = os.path.join('data', 'train_uw') # or 'train' !!!!!!
+dataPath = os.path.join('data', 'train_uw' 'class4') # or 'train' !!!!!!
 audioPathList = os.path.join(dataPath, 'audio')
 labelPathList = os.path.join(dataPath, 'label')
 
